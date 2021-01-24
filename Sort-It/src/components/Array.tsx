df2a97ff59cfb8ced7a333b2437e60f2e3c4e31a
bbastanza <b.bastanza@gmail.com>
2021-01-24 10:58:21 -0800

    commit css/ add array component

---
+++ Sort-It/src/components/Array.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Array() {
+    const array: number[] = [9, 7, 2, 1, 4, 8, 0, 3, 6, 5];
+    return (
+        <div>
+            <h1>{array}</h1>
+        </div>
+    );
+}

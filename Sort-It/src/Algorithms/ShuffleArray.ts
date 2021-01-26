936277316879d07fc84f6993f848493f96ca1582
bbastanza <b.bastanza@gmail.com>
2021-01-25 21:30:10 -0800

    commit add chart and try simple reverse with timeout
    need to figure out a way to set timeout and to visualize. Maybe the use of promises.

---
+++ Sort-It/src/Algorithms/ShuffleArray.ts
@@ -0,0 +1,11 @@
+export function shuffleArray(array: number[]) {
+    let currentIdx: number = array.length;
+    while (currentIdx !== 0) {
+        let randomIdx: number = Math.floor(Math.random() * currentIdx);
+        currentIdx -= 1;
+        let tmp: number = array[currentIdx];
+        array[currentIdx] = array[randomIdx];
+        array[randomIdx] = tmp;
+    }
+    return array;
+}
--- Sort-It/src/components/Array.tsx
+++ Sort-It/src/components/Array.tsx
@@ -1,10 +1,94 @@
-import React from "react";
+import { useState } from "react";
+import { Bar } from "react-chartjs-2";
+import { shuffleArray } from "../Algorithms/ShuffleArray";
 
 export default function Array() {
-    const array: number[] = [9, 7, 2, 1, 4, 8, 0, 3, 6, 5];
+    const [shuffledArray, setShuffledArray] = useState<number[]>([3, 6, 2, 7, 8, 5, 1, 9, 4, 10]);
+    const [sortType, setSortType] = useState<string>("simple");
+
+    function changeSize(size: number) {
+        const newArray: number[] = [];
+        for (let i = 1; i <= size; i++) {
+            newArray.push(i);
+        }
+        setShuffledArray(shuffleArray(newArray));
+    }
+
+    async function reverseArray() {
+        const newArray: number[] = shuffledArray;
+        console.log(newArray);
+        let firstIdx = 0;
+        let lastIdx = newArray.length - 1;
+        while (firstIdx < lastIdx) {
+                await new Promise(resolve => setTimeout(function () {
+                    let temp = newArray[firstIdx];
+                    newArray[firstIdx] = newArray[lastIdx];
+                    newArray[lastIdx] = temp;
+                    firstIdx++;
+                    lastIdx--;
+                    console.log(resolve)
+                    return resolve;
+                }, 300))
+        }
+    }
+
+    function buttonClass(type: string) {
+        return type === sortType ? "btn btn-primary" : "btn btn-info";
+    }
+
+    const chartData = {
+        labels: shuffledArray,
+        datasets: [
+            {
+                label: "value",
+                data: shuffledArray,
+                backgroundColor: "#377E86",
+            },
+        ],
+    };
+
+    const chartOptions = {
+        title: { display: true, text: sortType, fontSize: 25 },
+        maintainAspectRatio: false,
+    };
+
     return (
         <div>
-            <h1>{array}</h1>
+            <div style={{ width: "70vw", height: "60vh", display: "flex", justifyContent: "center", margin: "auto" }}>
+                <Bar data={chartData} options={chartOptions} />
+            </div>
+            <br />
+            <h2>Array Size</h2>
+            <input
+                type="range"
+                min="10"
+                max="310"
+                step="25"
+                value={shuffledArray.length}
+                className="slider"
+                onChange={e => changeSize(parseInt(e.target.value))}
+            />
+            <br />
+            <br />
+            <h2 style={{ textTransform: "capitalize" }}>{sortType} Sort</h2>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <button className={buttonClass("simple")} onClick={() => setSortType("simple")}>
+                    Simple Sort
+                </button>
+                <button className={buttonClass("bubble")} onClick={() => setSortType("bubble")}>
+                    Bubble Sort
+                </button>
+                <button className={buttonClass("selection")} onClick={() => setSortType("selection")}>
+                    Selection Sort
+                </button>
+                <button className={buttonClass("merge")} onClick={() => setSortType("merge")}>
+                    Merge Sort
+                </button>
+                <button className={buttonClass("quick")} onClick={() => setSortType("quick")}>
+                    Quick Sort
+                </button>
+            </div>
+            <button onClick={reverseArray}>click me!</button>
         </div>
     );
 }

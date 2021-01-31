db9c9764bf6d64bcfd42d3d9af5d826f57c1c145
bbastanza <b.bastanza@gmail.com>
2021-01-31 15:43:06 -0800

    commit use spread to set reversed array

--- Sort-It/src/components/Array.tsx
+++ Sort-It/src/components/Array.tsx
@@ -1,10 +1,10 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Bar } from "react-chartjs-2";
-import { shuffleArray } from "../Algorithms/ShuffleArray";
+import { shuffleArray } from "../Algorithms/shuffleArray";
 
 export default function Array() {
     const [sortType, setSortType] = useState<string>("simple");
-    const [shuffledArray, setShuffledArray] = useState<number[]>([
+    const [dataArray, setDataArray] = useState<number[]>([
         3,
         6,
         2,
@@ -16,45 +16,37 @@ export default function Array() {
         4,
         10,
     ]);
-    const arrayRef = useRef<number[]>([]);
-    arrayRef.current = shuffledArray;
-
     const [chartData, setChartData] = useState<object>({
-        labels: arrayRef.current,
+        labels: dataArray,
         datasets: [
             {
                 label: "value",
-                data: arrayRef.current,
+                data: dataArray,
                 backgroundColor: "#377E86",
             },
         ],
     });
+    const arrayRef = useRef<number[]>([]);
+    arrayRef.current = dataArray;
 
-    function reverseArray() {
-        const newArray: number[] = shuffledArray;
-        console.log("before", newArray);
-
-        let firstIdx: number = 0;
-        let lastIdx: number = newArray.length - 1;
-        while (firstIdx < lastIdx) {
-            let temp = newArray[firstIdx];
-            newArray[firstIdx] = newArray[lastIdx];
-            newArray[lastIdx] = temp;
-            firstIdx++;
-            lastIdx--;
-        }
-
+    // TODO get useEffect to fire
+    useEffect(() => {
+        console.log("useEffect fired");
         setChartData({
-            labels: newArray,
+            labels: arrayRef.current,
             datasets: [
                 {
                     label: "value",
-                    data: newArray,
+                    data: arrayRef.current,
                     backgroundColor: "#377E86",
                 },
             ],
         });
-        setShuffledArray(newArray);
+    }, [dataArray]);
+
+    function reverseArray() {
+        const newArray: number[] = dataArray;
+        setDataArray([...newArray.reverse()]);
     }
 
     function changeSize(size: number) {
@@ -62,7 +54,7 @@ export default function Array() {
         for (let i = 1; i <= size; i++) {
             newArray.push(i);
         }
-        setShuffledArray(shuffleArray(newArray));
+        setDataArray([...shuffleArray(newArray)]);
     }
 
     function buttonClass(type: string) {
@@ -93,7 +85,7 @@ export default function Array() {
                 min="10"
                 max="310"
                 step="25"
-                value={shuffledArray.length}
+                value={dataArray.length}
                 className="slider"
                 onChange={(e) => changeSize(parseInt(e.target.value))}
             />
@@ -127,7 +119,19 @@ export default function Array() {
                     Quick Sort
                 </button>
             </div>
-            <button onClick={reverseArray}>click me!</button>
+            <button onClick={() => reverseArray()}>click me!</button>
+            <button onClick={() => console.log(dataArray)}>
+                click me!
+            </button>
         </div>
     );
 }
+// let firstIdx: number = 0;
+// let lastIdx: number = newArray.length - 1;
+// while (firstIdx < lastIdx) {
+//     let temp = newArray[firstIdx];
+//     newArray[firstIdx] = newArray[lastIdx];
+//     newArray[lastIdx] = temp;
+//     firstIdx++;
+//     lastIdx--;
+// }

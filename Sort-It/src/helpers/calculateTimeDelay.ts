4d1a8dca01af1d96ba04a919e79f0cf682df12a3
bbastanza <b.bastanza@gmail.com>
2021-02-06 11:45:07 -0800

    commit change mergesort time delays/ add mdimages

--- Sort-It/src/helpers/calculateTimeDelay.ts
+++ Sort-It/src/helpers/calculateTimeDelay.ts
@@ -1,7 +1,24 @@
 export function calculateTimeDelay(length: number, sortType: string): number {
     let timeDelay: number;
-    if (sortType === "merge") timeDelay = 30;
-    else {
+    if (sortType === "merge" || sortType === "quick") {
+        switch (true) {
+            case length <= 25:
+                timeDelay = 80;
+                break;
+            case length <= 45:
+                timeDelay = 40;
+                break;
+            case length <= 65:
+                timeDelay = 20;
+                break;
+            case length <= 85:
+                timeDelay = 5;
+                break;
+            default:
+                timeDelay = 2;
+                break;
+        }
+    } else {
         switch (true) {
             case length <= 25:
                 timeDelay = 80;
